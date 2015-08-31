eb1883bc9f00f7b3d84ce2aeb4a3b1927b34230d
zjq383688496 <zjq383688496@gmail.com>
2015-09-01 02:00:20 +0800

    测试demo

---
+++ js/config.js
@@ -0,0 +1,9 @@
+require.config({
+	baseUrl: 'js',
+	paths: {
+		'jquery': 'jquery-2.1.4.min',
+		'jend': 'jend',
+		'angular': 'angular.min',
+		'route': 'angular-route.min'
+	}
+});
---
+++ js/index.js
@@ -0,0 +1,44 @@
+'jquery jend route';
+require(['angular'], function () {
+	//var ngApp = angular.module('ngApp',['ngRoute']);
+	var ngApp = angular.module('ngApp',[]);
+	ngApp.directive('hello', function () {
+		return {
+			restrict: 'A',
+			templateUrl: 'header.html',
+			controller: 'CountryListCtrl'
+		}
+	});
+	ngApp.controller('CountryListCtrl', function ($scope, $http){
+		console.log('13585575688')
+	});
+	ngApp.controller('NameCtrl', function ($scope) {
+		$scope.wahaha = function () {
+			console.log(JEND.page)
+		}
+	});
+	require(['jquery', 'jend'], function () {
+		/*ngApp = angular.module('ngApp',['ngRoute']);
+		console.log(ngApp.config)
+		ngApp.config(function($routeProvider) {
+			$routeProvider.
+				when('/', {
+					templateUrl: 'header.html',
+					controller: 'CountryListCtrl'
+				}).
+				otherwise({
+					redirectTo: '/'
+				});
+		});
+		ngApp.controller('CountryListCtrl', function ($scope, $http){
+			console.log('13585575688')
+		});
+		ngApp.controller('NameCtrl', function ($scope) {
+			$scope.wahaha = function () {
+				console.log(JEND.page)
+			}
+		});*/
+		//console.log(ngApp)
+		//console.log('13585575688'.isMobile())
+	})
+})
